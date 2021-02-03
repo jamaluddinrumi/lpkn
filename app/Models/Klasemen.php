fec571337b6d0c25a09dcd619932073a4e498aff
Jamaluddin Rumi <jamal.rumi@icloud.com>
2021-02-03 14:07:02 +0700

    Delete unused Models & Migrations

--- app/Models/Klasemen.php
+++ app/Models/Klasemen.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Passport\HasApiTokens;
 
 class Klasemen extends Model
 {
-    use HasFactory;
+    use HasFactory, HasApiTokens;
 
     protected $table = 'klasemens';
 
--- app/Models/Pertandingan.php
+++ app/Models/Pertandingan.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Passport\HasApiTokens;
 
 class Pertandingan extends Model
 {
-    use HasFactory;
+    use HasFactory, HasApiTokens;
 
     protected $table = 'pertandingans';
 
